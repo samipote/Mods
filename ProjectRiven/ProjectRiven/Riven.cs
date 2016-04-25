58b41df054638505a23e3ea82b362552966d5c9f
samipote <samipote2@gmail.com>
2016-04-25 00:28:37 -0400

    Added files via upload

---
+++ ProjectRiven/ProjectRiven/Riven.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+
+namespace ProjectRiven
+{
+    internal class Riven
+    {
+        public static Text Text = new Text("", new Font(FontFamily.GenericSansSerif, 9, FontStyle.Bold));
+        public static Spell.Active Q = new Spell.Active(SpellSlot.Q, 300);
+        public static Spell.Active E = new Spell.Active(SpellSlot.E, 325);
+
+        public static Spell.Skillshot R = new Spell.Skillshot(SpellSlot.R, 900, SkillShotType.Cone, 250, 1600, 45)
+        {
+            AllowedCollisionCount = int.MaxValue
+        };
+
+        public static Menu Menu, ComboMenu, HarassMenu, FarmMenu, MiscMenu;
+
+        public static Spell.Active W
+        {
+            get
+            {
+                return new Spell.Active(SpellSlot.W,
+                    (uint)
+                        (70 + Player.Instance.BoundingRadius +
+                         (Player.Instance.HasBuff("RivenFengShuiEngine") ? 195 : 120)));
+            }
+        }
+
+        public static bool IsRActive
+        {
+            get
+            {
+                return ComboMenu["forcedRKeybind"].Cast<KeyBind>().CurrentValue &&
+                       ComboMenu["Combo.R"].Cast<CheckBox>().CurrentValue;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.Hero != Champion.Riven) return;
+            Menu = MainMenu.AddMenu("Project:Riven", "projRiven");
+            Menu.AddLabel("a simpler riven");
+
+            ComboMenu = Menu.AddSubMenu("Combo Settings", "comboSettings");
+            ComboMenu.Add("Combo.Q", new CheckBox("Use Q"));
+            ComboMenu.Add("Combo.W", new CheckBox("Use W"));
+            ComboMenu.Add("Combo.E", new CheckBox("Use E"));
+            ComboMenu.Add("Combo.R2", new CheckBox("Use R Wave (Killable)"));
+            ComboMenu.AddLabel("R1 Settings");
+            ComboMenu.Add("Combo.R", new CheckBox("Use R"));
+            ComboMenu.Add("forcedRKeybind", new KeyBind("Forced R", false, KeyBind.BindTypes.PressToggle, 'T'));
+            ComboMenu.AddLabel("When To use R");
+            ComboMenu.Add("Combo.RCombo", new CheckBox("Cant Kill with Combo"));
+            ComboMenu.Add("Combo.RPeople", new CheckBox("Have more than 1 person near"));
+
+            HarassMenu = Menu.AddSubMenu("Harass Settings", "harassSettings");
+            HarassMenu.Add("Harass.Q", new CheckBox("Use Q"));
+            HarassMenu.Add("Harass.W", new CheckBox("Use W"));
+            HarassMenu.Add("Harass.E", new CheckBox("Use E"));
+
+            FarmMenu = Menu.AddSubMenu("Farm Settings", "farmSettings");
+            FarmMenu.AddLabel("Last Hit");
+            FarmMenu.Add("LastHit.Q", new CheckBox("Use Q"));
+            FarmMenu.Add("LastHit.W", new CheckBox("Use W"));
+            FarmMenu.AddLabel("Wave Clear");
+            FarmMenu.Add("WaveClear.Q", new CheckBox("Use Q"));
+            FarmMenu.Add("WaveClear.W", new CheckBox("Use W"));
+            FarmMenu.AddLabel("Jungle");
+            FarmMenu.Add("Jungle.Q", new CheckBox("Use Q"));
+            FarmMenu.Add("Jungle.W", new CheckBox("Use W"));
+            FarmMenu.Add("Jungle.E", new CheckBox("Use E"));
+
+            MiscMenu = Menu.AddSubMenu("Misc Settings", "miscSettings");
+            MiscMenu.AddLabel("Keep Alive Settings");
+            MiscMenu.Add("Alive.Q", new CheckBox("Keep Q Alive"));
+            MiscMenu.Add("Alive.R", new CheckBox("Use R2 Before Expire"));
+            MiscMenu.AddLabel("Humanizer Settings");
+            MiscMenu.Add("HumanizerDelay", new Slider("Humanizer Delay (ms)", 0, 0, 300));
+
+
+            ItemHandler.Init();
+            EventHandler.Init();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnTick += Game_OnTick;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (ComboMenu["Combo.R"].Cast<CheckBox>().CurrentValue)
+            {
+                var pos = Drawing.WorldToScreen(Player.Instance.Position);
+                Text.Draw("Forced R: " + IsRActive, Color.AliceBlue, (int) pos.X - 45,
+                    (int) pos.Y + 40);
+            }
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateHandler.Combo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
+            {
+                StateHandler.LastHit();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateHandler.LaneClear();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                StateHandler.Jungle();
+            }
+        }
+    }
+}
---
+++ ProjectRiven/ProjectRiven/StateHandler.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace ProjectRiven
+{
+    internal class StateHandler
+    {
+        public static bool EnableR;
+
+        public static void Combo()
+        {
+            EnableR = false;
+            if (Orbwalker.IsAutoAttacking) return;
+
+            var target = TargetSelector.GetTarget(Riven.E.Range + Riven.W.Range + 200, DamageType.Physical);
+
+            if (Riven.R.IsReady() && Player.Instance.HasBuff("RivenFengShuiEngine") &&
+                Riven.ComboMenu["Combo.R2"].Cast<CheckBox>().CurrentValue)
+            {
+                if (
+                    EntityManager.Heroes.Enemies.Where(
+                        enemy =>
+                            enemy.IsValidTarget(Riven.R.Range) &&
+                            enemy.Health <
+                            Player.Instance.CalculateDamageOnUnit(enemy, DamageType.Physical,
+                                DamageHandler.RDamage(enemy))).Any(enemy => Riven.R.Cast(enemy)))
+                {
+                    return;
+                }
+            }
+
+            if (target == null) return;
+
+            if (Riven.ComboMenu["Combo.R"].Cast<CheckBox>().CurrentValue && Riven.R.IsReady() && !Player.Instance.HasBuff("RivenFengShuiEngine"))
+            {
+                if (Riven.ComboMenu["Combo.RCombo"].Cast<CheckBox>().CurrentValue &&
+                    target.Health > DamageHandler.ComboDamage(target, true)
+                    && target.Health < DamageHandler.ComboDamage(target)
+                    && target.Health > Player.Instance.GetAutoAttackDamage(target, true)*2 ||
+                    Riven.ComboMenu["Combo.RPeople"].Cast<CheckBox>().CurrentValue &&
+                    Player.Instance.CountEnemiesInRange(600) > 1 || Riven.IsRActive)
+                {
+                    if (Riven.E.IsReady())
+                    {
+                        EnableR = true;
+                    }
+                }
+            }
+
+            if (Riven.ComboMenu["Combo.E"].Cast<CheckBox>().CurrentValue && (target.Distance(Player.Instance) > Riven.W.Range || Riven.IsRActive && Riven.R.IsReady()) && Riven.E.IsReady())
+            {
+                Player.CastSpell(SpellSlot.E, target.Position);
+                return;
+            }
+
+            if (Riven.ComboMenu["Combo.W"].Cast<CheckBox>().CurrentValue &&
+                target.Distance(Player.Instance) <= Riven.W.Range && Riven.W.IsReady())
+            {
+                if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+                {
+                    ItemHandler.Hydra.Cast();
+                    return;
+                }
+                Player.CastSpell(SpellSlot.W);
+            }
+        }
+
+        public static void Harass()
+        {
+            if (Orbwalker.IsAutoAttacking) return;
+
+            var target = TargetSelector.GetTarget(Riven.E.Range + Riven.W.Range, DamageType.Physical);
+
+            if (target == null) return;
+
+            if (Riven.HarassMenu["Harass.E"].Cast<CheckBox>().CurrentValue &&
+                (target.Distance(Player.Instance) > Riven.W.Range &&
+                 target.Distance(Player.Instance) < Riven.E.Range + Riven.W.Range ||
+                 Riven.IsRActive && Riven.R.IsReady() &&
+                 target.Distance(Player.Instance) < Riven.E.Range + 265 + Player.Instance.BoundingRadius) &&
+                Riven.E.IsReady())
+            {
+                Player.CastSpell(SpellSlot.E, target.Position);
+                return;
+            }
+
+            if (Riven.HarassMenu["Harass.W"].Cast<CheckBox>().CurrentValue &&
+                target.Distance(Player.Instance) <= Riven.W.Range && Riven.W.IsReady())
+            {
+                if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+                {
+                    ItemHandler.Hydra.Cast();
+                    return;
+                }
+                Player.CastSpell(SpellSlot.W);
+            }
+        }
+
+        public static void LaneClear()
+        {
+            Orbwalker.ForcedTarget = null;
+            if (Orbwalker.IsAutoAttacking || EventHandler.LastCastQ + 260 > Environment.TickCount) return;
+            foreach (
+                var minion in EntityManager.MinionsAndMonsters.EnemyMinions.Where(a => a.IsValidTarget(Riven.W.Range)))
+            {
+                if (Riven.FarmMenu["WaveClear.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady() &&
+                    minion.Health <=
+                    Player.Instance.CalculateDamageOnUnit(minion, DamageType.Physical, DamageHandler.QDamage()))
+                {
+                    Player.CastSpell(SpellSlot.Q, minion.Position);
+                    return;
+                }
+                if (Riven.FarmMenu["WaveClear.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                    minion.Health <=
+                    Player.Instance.CalculateDamageOnUnit(minion, DamageType.Physical, DamageHandler.WDamage()))
+                {
+                    Player.CastSpell(SpellSlot.W);
+                    return;
+                }
+            }
+        }
+
+        public static void LastHit()
+        {
+            Orbwalker.ForcedTarget = null;
+            if (Orbwalker.IsAutoAttacking) return;
+
+            foreach (
+                var minion in EntityManager.MinionsAndMonsters.EnemyMinions.Where(a => a.IsValidTarget(Riven.W.Range)))
+            {
+                if (Riven.FarmMenu["LastHit.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady() &&
+                    minion.Health <=
+                    Player.Instance.CalculateDamageOnUnit(minion, DamageType.Physical, DamageHandler.QDamage()))
+                {
+                    Player.CastSpell(SpellSlot.Q, minion.Position);
+                    return;
+                }
+                if (Riven.FarmMenu["LastHit.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                    minion.Health <=
+                    Player.Instance.CalculateDamageOnUnit(minion, DamageType.Physical, DamageHandler.WDamage()))
+                {
+                    Player.CastSpell(SpellSlot.W);
+                    return;
+                }
+            }
+        }
+
+        public static void Jungle()
+        {
+            var minion =
+                EntityManager.MinionsAndMonsters.Monsters.OrderByDescending(a => a.MaxHealth)
+                    .FirstOrDefault(a => a.Distance(Player.Instance) < Player.Instance.GetAutoAttackRange(a));
+
+            if (minion == null) return;
+
+            if (Riven.FarmMenu["Jungle.E"].Cast<CheckBox>().CurrentValue && (!Riven.W.IsReady() && !Riven.Q.IsReady() || Player.Instance.HealthPercent < 20) && Riven.E.IsReady() &&
+                EventHandler.LastCastW + 750 < Environment.TickCount)
+            {
+                Player.CastSpell(SpellSlot.E, minion.Position);
+            }
+        }
+
+        public static void ComboAfterAa(Obj_AI_Base target)
+        {
+            if (Player.Instance.HasBuff("RivenFengShuiEngine") && Riven.R.IsReady() &&
+                Riven.ComboMenu["Combo.R2"].Cast<CheckBox>().CurrentValue)
+            {
+                if (Player.Instance.CalculateDamageOnUnit(target, DamageType.Physical, DamageHandler.RDamage(target)) + Player.Instance.GetAutoAttackDamage(target, true) > target.Health)
+                {
+                    Riven.R.Cast(target);
+                    return;
+                }
+            }
+            if (Riven.ComboMenu["Combo.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                Riven.W.IsInRange(target))
+            {
+                if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+                {
+                    ItemHandler.Hydra.Cast();
+                    return;
+                }
+                Player.CastSpell(SpellSlot.W);
+                return;
+            }
+            if (Riven.ComboMenu["Combo.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady())
+            {
+                Player.CastSpell(SpellSlot.Q, target.Position);
+                return;
+            }
+            if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+            {
+                ItemHandler.Hydra.Cast();
+            }
+        }
+
+        public static void HarassAfterAa(Obj_AI_Base target)
+        {
+            if (Riven.HarassMenu["Harass.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                Riven.W.IsInRange(target))
+            {
+                if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+                {
+                    ItemHandler.Hydra.Cast();
+                    return;
+                }
+                Player.CastSpell(SpellSlot.W);
+                return;
+            }
+            if (Riven.HarassMenu["Harass.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady())
+            {
+                Player.CastSpell(SpellSlot.Q, target.Position);
+                return;
+            }
+            if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+            {
+                ItemHandler.Hydra.Cast();
+            }
+        }
+
+        public static void LastHitAfterAa(Obj_AI_Base target)
+        {
+            var unitHp = target.Health - Player.Instance.GetAutoAttackDamage(target, true);
+            if (unitHp > 0)
+            {
+                if (Riven.FarmMenu["LastHit.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady() &&
+                    Player.Instance.CalculateDamageOnUnit(target, DamageType.Physical, DamageHandler.QDamage()) >
+                    unitHp)
+                {
+                    Player.CastSpell(SpellSlot.Q, target.Position);
+                    return;
+                }
+                if (Riven.FarmMenu["LastHit.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                    Riven.W.IsInRange(target) &&
+                    Player.Instance.CalculateDamageOnUnit(target, DamageType.Physical, DamageHandler.WDamage()) >
+                    unitHp)
+                {
+                    Player.CastSpell(SpellSlot.W);
+                }
+            }
+        }
+
+        public static void LaneClearAfterAa(Obj_AI_Base target)
+        {
+            var unitHp = target.Health - Player.Instance.GetAutoAttackDamage(target, true);
+            if (unitHp > 0)
+            {
+                if (Riven.FarmMenu["WaveClear.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady())
+                {
+                    Player.CastSpell(SpellSlot.Q, target.Position);
+                    return;
+                }
+                if (Riven.FarmMenu["WaveClear.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                    Riven.W.IsInRange(target))
+                {
+                    Player.CastSpell(SpellSlot.W);
+                    return;
+                }
+            }
+            else
+            {
+                List<Obj_AI_Minion> minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy,
+                    Player.Instance.Position, Riven.Q.Range).Where(a => a.NetworkId != target.NetworkId).ToList();
+                if (Riven.FarmMenu["WaveClear.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady() && minions.Any())
+                {
+                    Player.CastSpell(SpellSlot.Q, minions[0].Position);
+                    return;
+                }
+                minions = minions.Where(a => a.Distance(Player.Instance) < Riven.W.Range).ToList();
+                if (Riven.FarmMenu["WaveClear.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                    Riven.W.IsInRange(target) && minions.Any())
+                {
+                    Player.CastSpell(SpellSlot.W);
+                    return;
+                }
+            }
+        }
+
+        public static void JungleAfterAa(Obj_AI_Base target)
+        {
+            if (Riven.FarmMenu["Jungle.W"].Cast<CheckBox>().CurrentValue && Riven.W.IsReady() &&
+                Riven.W.IsInRange(target))
+            {
+                if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+                {
+                    ItemHandler.Hydra.Cast();
+                    return;
+                }
+                Player.CastSpell(SpellSlot.W);
+                return;
+            }
+            if (Riven.FarmMenu["Jungle.Q"].Cast<CheckBox>().CurrentValue && Riven.Q.IsReady())
+            {
+                Player.CastSpell(SpellSlot.Q, target.Position);
+                return;
+            }
+            if (ItemHandler.Hydra != null && ItemHandler.Hydra.IsReady())
+            {
+                ItemHandler.Hydra.Cast();
+            }
+        }
+    }
+}
